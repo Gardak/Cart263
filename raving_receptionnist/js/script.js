8507226113ee6e6afc5a9243110606ffb1649cf2
Gardak <55040620+Gardak@users.noreply.github.com>
2020-01-23 17:28:19 -0500

    A2: Secrets
    The class has been added, netx step is to fix the mouse over function

--- raving_receptionnist/js/script.js
+++ raving_receptionnist/js/script.js
@@ -7,9 +7,9 @@
 "use strict";
 
 let $spans;
-
-let secretFound = 0;
-const secretTotal = 9;
+let $secret;
+let secretsFound = 0;
+let secretsTotal;
 
 const intervalTime = 500;
 const riskRevealed = 0.1;
@@ -18,18 +18,28 @@ $(document).ready(setup);
 
 function setup() {
   console.log('setup');
+
   $spans = $('span');
+  $secret = $('secret');
+  secretsTotal = $('.secret').length;
+  console.log(secretsTotal);
+
   setInterval(update, intervalTime);
 
   $spans.on('click' , spanClick);
-
+  $secret.on('mouseover', )
 }
 
 function update() {
   console.log('Udated!');
 
   $spans.each(updateSpan);
+  $('.secretsTotal').text(secretsTotal);
+  $('.secretsFound').text(secretsFound);
+}
 
+function secretFound(){
+  $(this).addClass('.found');
 }
 
 function updateSpan() {
