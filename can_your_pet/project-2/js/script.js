b4d6b775b0949471b0b0a8fbe5c0c70af4d22e6d
Gardak <Gardak@users.noreply.github.com>
2020-03-11 18:55:49 -0400

    P2

---
+++ can_your_pet/project-2/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/********************************************************************
+
+Meat your pet
+Alex Lorrain
+
+Take care of your animal. Feed him, bathe him and speak to him.
+But be careful not to get attach too much.
+*********************************************************************/
