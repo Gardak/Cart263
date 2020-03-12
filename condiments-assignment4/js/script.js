3d57fb81a79093e447d38a2c607c5d9d3f02150b
Gardak <55040620+Gardak@users.noreply.github.com>
2020-03-12 17:24:54 -0400

    A4: step1-2
    Added both libraries and the phrase ads them to the mix. Next step is to refresh the page with the click.

--- condiments-assignment4/js/script.js
+++ condiments-assignment4/js/script.js
@@ -15,6 +15,9 @@ https://github.com/dariusk/corpora
 
 $(document).ready(function() {
 
+  //Wait for a click on the mousse to refresh the page
+  document.addEventListener("click",refreshPhrase);
+
   // The first thing we need to do is load the data we're going
   // to use to get random words.
   //
@@ -31,6 +34,10 @@ $(document).ready(function() {
 // This function gets called by getJSON when the data has been loaded.
 // The data itself will be in the 'data' argument as a JavaScript object.
 function gotData(data) {
+
+  //A variable to use data outside the function
+  let dataFile = data;
+
   // Now we select random elements from the three arrays inside
   // our JSON to get a random condiment, cat, and room. Then we add those
   // words onto our page by setting the text of the appropriate span.
@@ -39,11 +46,11 @@ function gotData(data) {
   // Get a random condiment from the condiments array in the JSON
   let condiment = getRandomElement(data.condiments);
   // Assume it's singular
-  let verb = 'is';
+  let verbCondiment = 'is';
   // Check if the last latter of the condiment is an 's'
   if (condiment.charAt(condiment.length - 1) === 's') {
     // If so, assume it's plural (this is a flawed assumption)
-    verb = 'are';
+    verbCondiment = 'are';
   }
 
   // Now the cat
@@ -52,10 +59,23 @@ function gotData(data) {
   // Same again for room
   let room = getRandomElement(data.rooms);
 
+  //check if it starts with a vowel
+  let verbRoom = 'a';
+  if (room.charAt(0) === 'a' || room.charAt(0) === 'e' || room.charAt(0) === 'i' || room.charAt(0) === 'o' || room.charAt(0) === 'u') {
+    // If so, assume it's plural (this is a flawed assumption)
+    verbRoom = 'an';
+  }
+
+  //Giving the cat something to smoke
+  let cannabis = getRandomElement(data.cannabis);
+
+  //Chosing the music the cat listens
+  let music = getRandomElement(data.genres);
+
   // Now we can construct our description with a template string
   // We have the basic structure of a sentence and we substitute in the
   // values we've just calculated
-  let description = `${condiment} ${verb} like a ${cat} in a ${room}.`;
+  let description = `${condiment} ${verbCondiment} like a ${cat} in ${verbRoom} ${room} smoking a blunt of ${cannabis} and listening to some ${music}.`;
 
   // Finally, we add it to the page and hey presto!
   $('body').append(description)
@@ -74,4 +94,9 @@ function dataError(request, text, error) {
 // Returns a random element from the array provided
 function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
+}
+
+function refreshPhrase(dataFile) {
+  gotData(dataFile);
+  console.log(condiment);
 }
