8d6ac04c3edf3ef1876f5af51a520485bfb48d46
Gardak <Gardak@users.noreply.github.com>
2020-03-21 15:53:14 -0400

    P3 : pet is setup
    the pet stats, management and display is setup, next step is to "animate" and setup a timer between actions

--- meat-your-pet/project-3/js/pet.js
+++ meat-your-pet/project-3/js/pet.js
@@ -5,27 +5,44 @@ class Pet{
     this.y = y;
 
     this.thirst = 30 * 60;
-    this.drink = this.thirst / 2;
+    this.maxThirst = this.thirst;
+    this.drink = this.thirst / 3;
+
     this.hunger = 20 * 60;
-    this.feed = this.hunger / 2;
+    this.maxHunger = this.hunger;
+    this.feed = this.hunger / 3;
+
     this.cleanliness = 45 * 60;
-    this.clean = this.cleanliness / 2;
+    this.maxCleanliness = this.cleanliness;
+    this.clean = this.cleanliness / 3;
+
+    this.statDrain = 1;
+    this.statMaxDrain = 3;
+
     this.evolution = 0;
 
+    this.petImg = loadImage('assets/images/chick.png')
+    this.petChirpImg = loadImage('assets/images/chick_chirp.png')
 
   }
 
   manageStats() {
     if (this.hunger > 0){
-      this.hunger -= 1;
+      this.hunger -= statDrain;
     }
 
     if (this.thirst > 0){
-      this.thirst -= 1;
+      this.thirst -= statDrain;
     }
 
     if (this.cleanliness > 0){
-      this.cleanliness -= 1;
+      this.cleanliness -= statDrain;
+    }
+
+    if (this.cleanliness <= 0){
+      this.statDrain = this.statMaxDrain;
+    } else {
+      this.statDrain = 1;
     }
   }
 
@@ -39,6 +56,33 @@ class Pet{
 
   clean() {
     this.cleanliness += this.clean;
+  }
+
+  display() {
+
+
+
+    this.thirstBar = map(this.thirst, 0, this.maxThirst, 0, width /5);
+    this.hungerBar = map(this.hunger, 0, this.maxHunger, 0, width /5);
+    this.cleanlinessBar = map(this.clean, 0, this.maxCleanliness, 0, width /5);
+
+    push();
+    fill(0, 230, 0);
+    rectMode(CORNER);
+    rect(width / 10, height - 50, this.hungerBar, 20);
+    pop();
+
+    push();
+    fill(0, 220, 230);
+    rectMode(CORNER);
+    rect(4*width / 10, height - 50, this.cleanlinessBar, 20);
+    pop();
+
+    push();
+    fill(20, 25, 230);
+    rectMode(CORNER);
+    rect(7*width / 10, height - 50, this.thirstBar, 20);
+    pop();
   }
 
   // hoppingAround
--- meat-your-pet/project-3/js/script.js
+++ meat-your-pet/project-3/js/script.js
@@ -9,6 +9,12 @@ Take care of your animal. Feed him, bathe him and speak to him.
 But be careful not to get attach too much.
 *********************************************************************/
 
+let gameState = "intro"
+let spaceBar = 32;
+let fKey = 70;
+let dKey = 68;
+let cKey = 67;
+
 // setup()
 //
 // Sets up the game
@@ -25,5 +31,91 @@ function setup() {
 
 
 function gameplay() {
+  if (gameState === "intro"){
 
+    introScreen();
+    if (keyIsDown(spaceBar)){
+        gameState = "game";
+    }
+
+  } else if (gameState === "game") {
+
+    pet.display();
+    pet.manageStats();
+    if (keyIsDown(fKey)){
+      pet.feed();
+    } else if (keyIsDown(dKey)) {
+      pet.drink();
+    } else if (keyIsDown(cKey)) {
+      pet.clean();
+    }
+
+  } else if (gameState === "gameOver") {
+    gameOverScreen();
+    if (keyIsDown(spaceBar)){
+        gameState = "intro";
+    }
+  }
+}
+
+introScreen() {
+  textAlign(CENTER, CENTER);
+
+      let title = "Meet your pet\n";
+
+      let text = "Raise your pet as good as you can\n";
+      text = text + "--Press space to start--";
+
+      let instruction = "Press F to feed your pet\n";
+      instruction = instruction + "Press D to give your pet something to drink\n";
+      instruction = instruction + "Press C to clean your pet\n";
+
+      push();
+      textSize(100);
+      strokeWeight(10);
+      stroke(200,70,30);
+      fill(250);
+      text(title, width / 2, height / 3);
+      pop();
+
+      push();
+      textSize(36);
+      strokeWeight(3);
+      stroke(250);
+      fill(200,70,30);
+      text(text, width / 2, height / 2);
+      pop();
+
+      push();
+      textSize(30);
+      noStroke();
+      fill(200,70,30);
+      text(instruction, width * 4/5, height * 4/5);
+      pop();
+
+}
+
+gameOverScreen() {
+  textAlign(CENTER, CENTER);
+
+      let title = "Poor pet\n";
+
+      let text = "You failed to take care of your pet\n";
+      text = text + "--Press space to restart--";
+
+      push();
+      textSize(100);
+      strokeWeight(10);
+      stroke(200,70,30);
+      fill(250);
+      text(title, width / 2, height / 3);
+      pop();
+
+      push();
+      textSize(36);
+      strokeWeight(3);
+      stroke(250);
+      fill(200,70,30);
+      text(text, width / 2, height / 2);
+      pop();
 }
